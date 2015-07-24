0f004e73387c59f8cbb1a663139bbc7370627b4f
Dan <dangarbage15@yahoo.com>
2015-07-24 15:31:31 +0000

    finished

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,14 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  
+  def submit
+    @location = params["place"]
+    if @location == "Chicago"
+      @message = "Wooho, you're awesome"
+    else
+      @message = "Oh...that's alright...I guess"
+    end
+    render 'message'
+  end
 end
